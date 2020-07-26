eb1b0005608e504502b2e1370c986bcedaf39c78
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-26 04:29:22 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,19 @@
-# Add your variables here
+first_number = 4
+second_number = 2
+
+
+first_number + second_number
+
+sum = 6
+
+first_number - second_number
+
+difference = 2
+
+first_number ** second_number
+
+product = 8
+
+first_number / second_number
+
+quotient = 2
