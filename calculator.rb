2ed5e4c0edcdb281b87b8527ca4865d9ee3f441e
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-26 04:28:43 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,3 +5,15 @@ second_number = 2
 first_number + second_number
 
 sum = 6
+
+first_number - second_number
+
+difference = 2
+
+first_number ** second_number
+
+product = 8
+
+first_number / second_number
+
+quotient = 2
